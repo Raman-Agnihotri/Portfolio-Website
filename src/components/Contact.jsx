19390b879b7e170a1a8c8b3d4fb8c8ee7dbe8ef5
Raman Agnihotri <raman@gmail.com>
2025-04-25 01:28:32 +0530

    Initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,12 +15,25 @@ const Contact = () => {
          data-aos="zoom-in-up"
 data-aos-duration="1000"
         >
-          <a href="#" className="items"><FaInstagramSquare className="icons" /></a>
-          <a href="#" className="items"><CiFacebook className="icons" /></a>
-          <a href="#" className="items"><FaLinkedin className="icons" /></a>
-          <a href="#" className="items"><FaSquareXTwitter className="icons" /></a>
-          <a href="https://github.com/Raman-Agnihotri" className="items"><FaGithub className="icons" /></a>
-          <a href="mailto:ramanagnihotrimact@gmail.com" className="items"><SiGmail className="icons" /></a>
+          <a href="https://www.instagram.com/raman_agnihotri_?igsh=MTd1Z3BxNjc5Znhh" className="items" target="_blank" rel="noopener noreferrer">
+  <FaInstagramSquare className="icons" />
+</a>
+<a href="https://www.facebook.com/raman.agnihotri.7564" className="items" target="_blank" rel="noopener noreferrer">
+  <CiFacebook className="icons" />
+</a>
+<a href="https://www.linkedin.com/in/raman-agnihotri?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="items" target="_blank" rel="noopener noreferrer">
+  <FaLinkedin className="icons" />
+</a>
+<a href="https://x.com/agnih120?t=f9cvr6QO86qC_qwhjKwU6w&s=09" className="items" target="_blank" rel="noopener noreferrer">
+  <FaSquareXTwitter className="icons" />
+</a>
+<a href="https://github.com/Raman-Agnihotri" className="items" target="_blank" rel="noopener noreferrer">
+  <FaGithub className="icons" />
+</a>
+<a href="mailto:ramanagnihotri.mact@gmail.com" className="items">
+  <SiGmail className="icons" />
+</a>
+
         </div>
       </div>
     </>
